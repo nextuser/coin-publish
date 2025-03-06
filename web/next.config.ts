d65f6c2134d0561c041deeac616a64b8c8da53e5
nextuser <nextuser@163.com>
2025-03-06 22:56:00 +0800

    ignore eslint

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds : true,
+    dirs: ['app', 'components','lib','contracts'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 };
 
 export default nextConfig;
